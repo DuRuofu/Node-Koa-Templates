8827312a6364f418c75e9b38f8926de35ca3d9eb
DuRuofu <duruofu@qq.com>
2024-01-12 13:26:28 +0800

    add(*):建立工程

---
+++ routes/index.ts
@@ -0,0 +1,23 @@
+// src/routes/index.ts
+
+import { Express, Request, Response, Router } from 'express'
+
+// 路由配置接口
+interface RouterConf {
+    path: string,
+    router: Router,
+    meta?: unknown
+  }
+
+// 路由配置
+const routerConf: Array<RouterConf> = []
+
+function routes(app: Express) {
+  // 根目录
+  app.get('/', (req: Request, res: Response) => res.status(200).send('Hello Shinp!!!'))
+
+  routerConf.forEach((conf) => app.use(conf.path, conf.router))
+}
+
+export default routes
+
